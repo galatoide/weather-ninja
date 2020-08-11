2035d2882d4c995cdbb546b98c5e79d0e9d6fbd7
Wilson Cruz <galatoide@gmail.com>
2020-08-11 16:38:32 +0100

    working

---
+++ scripts/app.js
@@ -0,0 +1,62 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+    const {cityDets, weather} = data;
+
+    // UPDATE DETAILS TEMPLATE
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    // UPDATE THE NIGHT/DAY & ICON IMAGES
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    // if (weather.IsDayTime) {
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg';
+    // }
+    time.setAttribute('src', timeSrc)
+
+    // REMOVE THE D-NONE CLASS IF PRESENT
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none')
+    }
+}
+
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return {
+        cityDets: cityDets,
+        weather: weather
+        // cityDets,
+        // weather
+    }
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // GET CITY VALUE
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // UPDATE THE UI WITH THE NEW CITY
+    updateCity(city)
+    .then(data => updateUI(data))
+    .catch(data => console.log(data));
+});
