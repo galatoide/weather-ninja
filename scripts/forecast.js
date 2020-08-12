5eb9c337cdaf47c8a18703d9b81cdd0d80569394
Wilson Cruz <galatoide@gmail.com>
2020-08-12 12:36:10 +0100

    change to class

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,3 +1,31 @@
+class Forecast{
+    constructor(){
+        this.key = 'pM0TtsVdCPg5g7lKUSHuOF2AMo6VzkRl';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+
+        return {cityDets, weather}
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+}
+
 const key = 'pM0TtsVdCPg5g7lKUSHuOF2AMo6VzkRl';
 
 //GET WEATHER INFO
